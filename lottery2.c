9b13274ebdf6c328e22899cb9f6a0ab5a9de80aa
amrina-rahman <amrinarahman04@gmail.com>
2025-04-03 22:16:56 +0600

    Add files via upload
    . This code is a simple lottery program that allows users to input participant names and randomly selects a winner. It includes error handling for invalid participant numbers and uses the `rand()` function to generate a random index for the winner.

---
+++ lottery2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define MAX_PARTICIPANTS 50
+
+int main() {
+    char participants[MAX_PARTICIPANTS][50];
+    int num, i ,draw;
+    //input number of participants
+    printf("welcome to Lucky Draw Winner Selector!\nEnter the number of participants: ");
+    scanf("%d", &num);
+    if (num > MAX_PARTICIPANTS || num <1)
+    {
+        printf("Invalid number of participants.");
+    }
+    //input names of participants
+        for ( i = 0; i < num; i++)
+    {
+        printf("Enter the name of participant %d: ", i + 1);
+        scanf("%s", participants[i]);
+    }
+    //randomly select a winner
+    srand(time(0));
+    draw = rand() % num;
+    printf("The winner is %s!!", participants[draw]);
+    
+    return 0;
+}
